5a43055efd123e04cfc80cab63ca00f0a4c982d9
alpertncc <alperen.tunc.02@gmail.com>
2023-08-05 23:35:54 +0300

    Chapter5.2 is updated.

--- rustBook/chapter5/exampleProgram/src/main.rs
+++ rustBook/chapter5/exampleProgram/src/main.rs
@@ -1,3 +1,5 @@
+/*
+
 // A program to calculate the area of a rectangle
 
 {
@@ -54,12 +56,51 @@
 
     fn area(rectangle: &Rectangle) -> u32 { 
         // we want to borrow the struct rather than take ownership of it. 
-        // This way, main retains its ownership and can continue using rect1, 
+        // This way, main function retains its ownership and can continue using rect1, 
         // which is the reason we use the & in the function signature and where we call the function.
         rectangle.width * rectangle.height
     }
 
 }
+
+*/
+
+
+// Adding Useful Functionality with `Derived Traits`
+
+    #[derive(Debug)] // You MUST add this line to be able to take output of the structs!
+    struct rectangle {
+        widht: u32,
+        height: u32,
+    }
+
+    
+    fn main() {
+        let rect5 = rectangle {
+            widht: 30,
+            height: 50,
+        };
+
+        // When you run this line: `println!("rectangle is {}", rect5);`
+        // you will take the error, "`rectangle` cannot be formatted with the default formatter"
+        // This is because, println! macro can display the defined traits before. 
+        // You should specify the display format! 
+
+        println!("rectange is {:?}",rect5); // The output will be `rectange is rectangle { widht: 30, height: 50 }`
+
+        //  When we have larger structs, it’s useful to have output that’s a bit easier to read;
+        //  in those cases, we can use ` println!("rectange is {:#?}",rect5);`. The output will be; 
+        /* `rect1 is Rectangle {
+                width: 30,
+                height: 50,
+            }`
+        */
+
+
+    }
+
+
+
 
 
 
